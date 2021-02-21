77ac5e0894e13bd0983f2b372ede312f567f080e
jaime <jaime@jaimes-MacBook.local>
2021-02-21 12:36:35 -0700

    working on local storage and screenshot

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,12 +4,12 @@ $("#currentDay").text(currentDate);
 var displayHour = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 for (var i = 9; i < 18; i++) {
     $('.container').append(`
-<div class = "row time-block" id = "hour-${i}"> 
+    <div class = "row time-block" id = "hour-${i}"> 
     <div class = "col-1 hour">${displayHour[i - 9]}</div>
     <textarea class="col-10 description"/>
-    <button class="fa fa-save saveBtn"></button>
-</div> 
-`)
+    <button class="col-1 fa fa-save saveBtn"></button>
+    </div> 
+    `)
     if (moment().format('H') == i) {
         $(`#hour-${i}`).addClass("present");
     } else if (moment().format('H') < i) {
@@ -17,12 +17,12 @@ for (var i = 9; i < 18; i++) {
     } else {
         $(`#hour-${i}`).addClass("past");
     }
-
-    localStorage.getItem(`#hour-${i}`)
 }
 $('.saveBtn').on("click", function () {
     var key = $(this).parent().attr('id');
     var value = $(this).siblings(".description").val();
     localStorage.setItem(key, value);
 })
-
+for (var i = 9; i < 18; i++) {
+    $(`#hour-${i}.description`).val(localStorage.getItem(`#hour-${i}`));
+}
